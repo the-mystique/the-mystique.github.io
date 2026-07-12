9018c3e109ce7fa7d6df0e8417b08aa9b26e75a9
Mystique <haochencaier142@gmail.com>
2026-07-12 20:48:00 +0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <div>
+      <h1>
+        Mystique
+      </h1>
+
+      <p>
+        The rise of the Dark Lord..
+      </p>
+    </div>
+  )
+
+}
+
+export default App
