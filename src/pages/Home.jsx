d3713f92add3d2610b0e83f2fdff31ef2e3a5e73
Mystique <haochencaier142@gmail.com>
2026-07-17 18:16:32 +0500

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import Hero from "../components/Hero/Hero";
+import GridBackground from "../components/GridBackground/GridBackground";
+import CursorGlow from "../components/CursorGlow/CursorGlow";
+
+export default function Home(){
+
+    return(
+
+        <>
+
+            <CursorGlow/>
+
+            <GridBackground/>
+
+            <Hero/>
+
+        </>
+
+    );
+
+}
