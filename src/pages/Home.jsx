a747dc39623e97fbe54a9703e77f267571d702f7
Mystique <haochencaier142@gmail.com>
2026-07-17 18:52:57 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,17 @@
-import Hero from "../components/Hero/Hero";
 import GridBackground from "../components/GridBackground/GridBackground";
 import CursorGlow from "../components/CursorGlow/CursorGlow";
+import Noise from "../components/Noise/Noise";
+import Hero from "../components/Hero/Hero";
 
 export default function Home(){
 
     return(
-
         <>
-
             <GridBackground/>
-
+            <Noise/>
             <CursorGlow/>
-
             <Hero/>
-
         </>
-
     );
 
 }
