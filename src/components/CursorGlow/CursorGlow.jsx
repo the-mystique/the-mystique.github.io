072bec907cbeb55b41b80dc25c25a8a7c9c81fe6
Mystique <haochencaier142@gmail.com>
2026-07-17 18:48:36 +0500

    Update CursorGlow.jsx

--- src/components/CursorGlow/CursorGlow.jsx
+++ src/components/CursorGlow/CursorGlow.jsx
@@ -1,7 +1,30 @@
 import "./CursorGlow.css";
+import { useEffect } from "react";
 
 export default function CursorGlow(){
 
-    return null;
+    useEffect(()=>{
+
+        const move=(e)=>{
+
+            document.documentElement.style.setProperty(
+                "--mouse-x",
+                `${e.clientX}px`
+            );
+
+            document.documentElement.style.setProperty(
+                "--mouse-y",
+                `${e.clientY}px`
+            );
+
+        };
+
+        window.addEventListener("mousemove",move);
+
+        return()=>window.removeEventListener("mousemove",move);
+
+    },[]);
+
+    return <div className="cursor-light"/>
 
 }
