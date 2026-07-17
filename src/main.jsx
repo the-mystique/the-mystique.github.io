0fb83f3725e0b09b39ca3bc395aef3198aeddc9e
Mystique <haochencaier142@gmail.com>
2026-07-17 18:15:20 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(
-  document.getElementById('root')
-).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import App from "./App";
+
+import "./styles/globals.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+
+);
